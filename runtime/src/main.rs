77b6d333047293d8233fa65a78cb0fe735c8680c
Ho Kim <ho.kim@ulagbulag.io>
2022-04-07 14:09:40 +0000

    Initial commit

---
+++ runtime/src/main.rs
@@ -0,0 +1,11 @@
+//! Example of instantiating of instantiating a wasm module which uses WASI
+//! imports.
+
+// You can execute this example with `cargo run --example wasi`
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    Ok(())
+}
