2c001294cd9c10f89041d6968640621d9e12971f
Ho Kim <ho.kim@ulagbulag.io>
2022-04-08 12:11:54 +0000

    Add a basic onnxruntime example

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -4,8 +4,54 @@
 // You can execute this example with `cargo run --example wasi`
 
 use anyhow::Result;
+use onnxruntime::{
+    environment::Environment, ndarray::Array, tensor::OrtOwnedTensor, GraphOptimizationLevel,
+    LoggingLevel,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let environment = Environment::builder()
+        .with_name("test")
+        // The ONNX Runtime's log level can be different than the one of the wrapper crate or the application.
+        .with_log_level(LoggingLevel::Info)
+        .build()?;
+
+    let mut session = environment
+        .new_session_builder()?
+        .with_optimization_level(GraphOptimizationLevel::Basic)?
+        .with_number_threads(1)?
+        // NOTE: The example uses SqueezeNet 1.0 (ONNX version: 1.3, Opset version: 8).
+        //       Obtain it with:
+        //          curl -LO "https://media.githubusercontent.com/media/onnx/models/main/vision/classification/squeezenet/model/squeezenet1.0-8.onnx"
+        .with_model_from_file("squeezenet1.0-12-int8.onnx")?;
+
+    let input0_shape: Vec<usize> = session.inputs[0].dimensions().map(|d| d.unwrap()).collect();
+    let output0_shape: Vec<usize> = session.outputs[0]
+        .dimensions()
+        .map(|d| d.unwrap())
+        .collect();
+
+    assert_eq!(input0_shape, [1, 3, 224, 224]);
+    assert_eq!(output0_shape, [1, 1000, 1, 1]);
+
+    // initialize input data with values in [0.0, 1.0]
+    let n: u32 = session.inputs[0]
+        .dimensions
+        .iter()
+        .map(|d| d.unwrap())
+        .product();
+    let array = Array::linspace(0.0_f32, 1.0, n as usize)
+        .into_shape(input0_shape)
+        .unwrap();
+    let input_tensor_values = vec![array];
+
+    let outputs: Vec<OrtOwnedTensor<f32, _>> = session.run(input_tensor_values)?;
+
+    assert_eq!(outputs[0].shape(), output0_shape.as_slice());
+    for i in 0..5 {
+        println!("Score for class [{}] =  {}", i, outputs[0][[0, i, 0, 0]]);
+    }
+
     Ok(())
 }
