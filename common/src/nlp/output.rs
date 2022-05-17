8490b43a15d8d240bce943da5d046fae13f377e7
Ho Kim <ho.kim@ulagbulag.io>
2022-05-17 16:08:27 +0000

    Add SentenceLabel

---
+++ common/src/nlp/output.rs
@@ -0,0 +1,11 @@
+use bytecheck::CheckBytes;
+use rkyv::{Archive, Deserialize, Serialize};
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Archive, Serialize, Deserialize)]
+#[archive(compare(PartialEq))]
+#[archive_attr(derive(CheckBytes, Copy, Clone, Debug, PartialEq, Eq, Hash))]
+pub enum SentenceLabel {
+    Contradiction,
+    Entailment,
+    Neutral,
+}
